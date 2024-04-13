55e5f7e16f75df8f35fb73443a8cc5474666f879
uezcr <75730965+uezcr@users.noreply.github.com>
2024-04-13 20:30:22 +0800

    测试新模型

---
+++ Plugins/GameFeatures/TPSGameAI/Source/TPSGameAIRuntime/TPSGameAIRuntime.Build.cs
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TPSGameAIRuntime : ModuleRules
+{
+	public TPSGameAIRuntime(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"TOUGame",
+				"ModularGameplay",
+				"CommonGame"
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"GameplayTags",
+				"GameplayTasks",
+				"GameplayAbilities",
+				"GameplayMessageRuntime",
+				"CommonUI",
+				"UMG",
+				"DataRegistry",
+				"AsyncMixin",
+				"EnhancedInput",
+				"GameSubtitles",
+				"DeveloperSettings",
+				"AIModule"
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
